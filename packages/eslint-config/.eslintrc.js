e5fed56109398079bd1fa11bbc9adddd07f036d0
Squibs <z.r.holman+github@gmail.com>
2021-02-13 15:32:17 -0700

    1.0.3 adjustments, added es2021 environment.

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -1,9 +1,13 @@
 var config = {
   env: {
     browser: true,
+    es2021: true,
     node: true,
   },
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  extends: [
+    "airbnb-base",
+    "plugin:prettier/recommended"
+  ],
   rules: {
     "func-names": ["error", "never"],
   },
