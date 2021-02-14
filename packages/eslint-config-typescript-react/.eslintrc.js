901ea80103b8cc9ecae963990842021211c191a2
Squibs <z.r.holman+github@gmail.com>
2021-02-13 22:06:17 -0700

    1.0.0-alpha.0 eslint-config-typescript-react

---
+++ packages/eslint-config-typescript-react/.eslintrc.js
@@ -0,0 +1,23 @@
+var config = {
+  parser: "@typescript-eslint/parser",
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+  ],
+  rules: {
+    "react/state-in-constructor": "off",
+    "react/jsx-one-expression-per-line": 2,
+    "@typescript-eslint/member-delimiter-style": 2,
+  },
+};
+
+module.exports = config;
