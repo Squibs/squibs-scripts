43d6e3aa9ec8bf241a61a13519c4b5297f60e456
Squibs <z.r.holman+github@gmail.com>
2021-02-18 14:20:39 -0700

    1.0.1 Disable prefer-default-export eslint rule.

--- packages/eslint-config-typescript-react/.eslintrc.js
+++ packages/eslint-config-typescript-react/.eslintrc.js
@@ -1,22 +1,23 @@
 var config = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
   extends: [
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "prettier/react",
-    "prettier/@typescript-eslint",
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/@typescript-eslint',
   ],
   rules: {
-    "react/state-in-constructor": "off",
-    "react/jsx-one-expression-per-line": 2,
-    "@typescript-eslint/member-delimiter-style": 2,
+    'react/state-in-constructor': 'off',
+    'react/jsx-one-expression-per-line': 2,
+    '@typescript-eslint/member-delimiter-style': 2,
+    'import/prefer-default-export': 'off',
   },
 };
 
