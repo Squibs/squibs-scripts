53bfd46672135a4240dcb6782adfcbffc86365f8
Squibs <z.r.holman+github@gmail.com>
2022-09-21 17:06:53 -0600

    1.0.1 Gently copy .prettierignore to root.

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,1 +1,20 @@
+/**
+ * Script to run after npm install
+ *
+ * Copy selected files to user's directory
+ */
+('use strict');
+
+var gentlyCopy = require('gently-copy');
+
+var filesToCopy = ['.prettierignore'];
+
+// User's local directory
+// Warning: This assumes the package is installed into `node_modules/<@somescope>/<package-name>/`
+var userPath = '../../../';
+
+// Moving files to user's local directory
+gentlyCopy(filesToCopy, userPath, { overwrite: true });
+
+// to allow extending of prettier config
 module.exports = require('./.prettierrc.json');
