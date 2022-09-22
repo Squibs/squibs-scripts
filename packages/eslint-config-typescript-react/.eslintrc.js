ce7f4665c243023556434735baa01b7c5da504c8
Squibs <z.r.holman+github@gmail.com>
2022-09-21 22:20:43 -0600

    2.0.2 Additional rule to prefer arrow functions.

--- packages/eslint-config-typescript-react/.eslintrc.js
+++ packages/eslint-config-typescript-react/.eslintrc.js
@@ -18,6 +18,13 @@ var config = {
     'react/jsx-one-expression-per-line': 2,
     '@typescript-eslint/member-delimiter-style': 2,
     'import/prefer-default-export': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      }
+    ],
   },
 };
 
