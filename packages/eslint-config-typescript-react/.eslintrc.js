937b7b93f12ea197629b1794cd618d327f9fb3d5
Squibs <z.r.holman+github@gmail.com>
2022-09-23 12:15:04 -0600

    2.0.3 Programmatic rule override for redux toolkit

--- packages/eslint-config-typescript-react/.eslintrc.js
+++ packages/eslint-config-typescript-react/.eslintrc.js
@@ -1,3 +1,20 @@
+var fs = require('fs');
+var path = require('path');
+var myOverrides = [];
+
+// if you are using redux toolkit, change the no-param-reassign eslint rule to allow state mutation
+if (fs.existsSync(path.join(__dirname, '..', '..', '@reduxjs', 'toolkit'))) {
+  console.log('@squibs/eslint-config-typescript-react -> node_modules/@reduxjs/toolkit exists -> ignoring modification to state');
+  myOverrides = [
+    {
+      files: ['src/**/store/*/*.ts', 'src/**/store/*/*.js'],
+      rules: {
+        'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['state'] }]
+      }
+    }
+  ];
+}
+
 var config = {
   parser: '@typescript-eslint/parser',
   env: {
@@ -26,6 +43,7 @@ var config = {
       }
     ],
   },
+  overrides: myOverrides,
 };
 
 module.exports = config;
